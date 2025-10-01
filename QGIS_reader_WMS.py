3a8541577fccce2fa675cb836820f5b8f50a3336
POLIKbuilt <danilkach73@gmail.com>
2025-10-02 00:44:28 +0200

    WMS tests completed with success

--- QGIS_reader_WMS.py
+++ QGIS_reader_WMS.py
@@ -1,5 +1,6 @@
 from qgis.core import *
 
+# WMS working, only needs correct data
 wms_url = "crs=CRS:84&dpiMode=7&format=image/png&layers=2&styles&url=https://zbgisws.skgeodesy.sk/zbgis_administrativne_hranice_wms_featureinfo/service.svc/get"
 wms_layer = QgsRasterLayer(wms_url, "WMS Example", "wms")
 
