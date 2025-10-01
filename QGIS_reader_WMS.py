98558b07e6f06e9dc10093c0fdfcdd335f6bf52d
POLIKbuilt <danilkach73@gmail.com>
2025-10-02 00:42:55 +0200

    GUI turned off

--- QGIS_reader_WMS.py
+++ QGIS_reader_WMS.py
@@ -1,10 +1,6 @@
 from qgis.core import *
 
-qgs = QgsApplication([], False)
-qgs.initQgis()
-
 wms_url = "crs=CRS:84&dpiMode=7&format=image/png&layers=2&styles&url=https://zbgisws.skgeodesy.sk/zbgis_administrativne_hranice_wms_featureinfo/service.svc/get"
-
 wms_layer = QgsRasterLayer(wms_url, "WMS Example", "wms")
 
 if wms_layer.isValid():
@@ -13,4 +9,3 @@ if wms_layer.isValid():
 else:
     print("WMS failed")
 
-qgs.exitQgis()
