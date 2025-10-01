852cacdcc8a534800818f885193402dd9829b58e
POLIKbuilt <danilkach73@gmail.com>
2025-10-02 00:38:40 +0200

    WMS raster tests, add xml for WMS

--- QGIS_reader_WMS.py
+++ QGIS_reader_WMS.py
@@ -3,13 +3,7 @@ from qgis.core import *
 qgs = QgsApplication([], False)
 qgs.initQgis()
 
-wms_url = (
-    "contextualWMSLegend=0&"
-    "crs=EPSG:3857&"
-    "format=image/png&"
-    "layers=ne:NE1_HR_LC_SR_W_DR&"
-    "url=https://zbgisws.skgeodesy.sk/zbgis_wms_featureinfo/service.svc/get"
-)
+wms_url = "crs=CRS:84&dpiMode=7&format=image/png&layers=2&styles&url=https://zbgisws.skgeodesy.sk/zbgis_administrativne_hranice_wms_featureinfo/service.svc/get"
 
 wms_layer = QgsRasterLayer(wms_url, "WMS Example", "wms")
 
