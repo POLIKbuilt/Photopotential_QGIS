3048cad22569da3fc252177e89bf3d15c3928959
POLIKbuilt <danilkach73@gmail.com>
2025-10-02 12:38:11 +0200

    adding coordinate boxing for wms

--- QGIS_reader_WMS.py
+++ QGIS_reader_WMS.py
@@ -6,6 +6,12 @@ layer_name = "WMS_layer"
 wms_url = "crs=CRS:84&dpiMode=7&format=image/png&layers=0&styles&url=https://zbgisws.skgeodesy.sk/zbgis_dmr_wms/service.svc/get"
 wms_layer = QgsRasterLayer(wms_url, layer_name, "wms")
 
+def coordinate_boxing(xMin, xMax, yMin, yMax):
+    extender = QgsRectangle(xMin, yMin, xMax, yMax)
+    layer.setRenderer(renderer)
+    layer.triggerRepaint()
+
+
 if wms_layer.isValid():
     project = QgsProject.instance()
     for layer in project.mapLayers().values():
@@ -15,6 +21,7 @@ if wms_layer.isValid():
     working_layer = project.addMapLayer(wms_layer)
     print("WMS loaded successfully")
     render_set(working_layer, 315, 45)
+    coordinate_boxing(823000, 5930000, 833000, 5940000)
 else:
     print("WMS failed")
 
