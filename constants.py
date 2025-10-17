06b7e9410c0071b6ae22509ef5a9bd25187c8709
POLIKbuilt <danilkach73@gmail.com>
2025-10-18 00:39:18 +0200

    changed format of coordinates

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 LAYER_NAME = "Working layer"
-OUTPUT_LAYER = r"data/output.tif"
+OUTPUT_LAYER = "output.tif"
 
 ZONE_LAT = 48.198583  # 48°11'54.9"N
 ZONE_LON = 17.055028  # 17°03'18.1"E
@@ -7,4 +7,4 @@ RADIUS = 5000  # 5 km
 
 # new boxing coordinates (two points)
 X1, Y1 = 48.126167, 17.085511
-X2, Y2 = 48.114157,17.095793
+X2, Y2 = 48.114157, 17.095793
