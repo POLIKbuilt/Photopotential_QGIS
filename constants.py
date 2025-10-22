2b58e1d2e15fc202a3086c55017c4f7763a7f17f
POLIKbuilt <danilkach73@gmail.com>
2025-10-22 08:28:23 +0200

    moved x,y from constants.py to global variables

--- constants.py
+++ constants.py
@@ -5,6 +5,3 @@ ZONE_LAT = 48.198583  # 48°11'54.9"N
 ZONE_LON = 17.055028  # 17°03'18.1"E
 RADIUS = 5000  # 5 km
 
-# new boxing coordinates (two points)
-X1, Y1 = 48.126167, 17.085511
-X2, Y2 = 48.114157, 17.095793
