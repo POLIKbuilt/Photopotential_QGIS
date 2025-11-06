9a2e4732427ba3967fb2c71fab03e82ceff95cb9
POLIKbuilt <danilkach73@gmail.com>
2025-11-06 01:40:07 +0100

    WMS_load constant added

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 LAYER_NAME = "Working layer"
-OUTPUT_LAYER = "output.tif"
+OUTPUT_LAYER = r"data\output.tif"
+WMS_LOAD_PATH = "data\WMS_load.tif"
 
 ZONE_LAT = 48.198583  # 48°11'54.9"N
 ZONE_LON = 17.055028  # 17°03'18.1"E
