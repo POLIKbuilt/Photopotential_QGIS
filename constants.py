00a72d0cc1d319fbbab03864ce6787a5fc48dfdc
POLIKbuilt <danilkach73@gmail.com>
2025-11-13 02:51:14 +0100

    test path const added

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 LAYER_NAME = "Working layer"
+TEST_PATH = r"data\test_raster.tif"
 OUTPUT_LAYER = r"data\output.tif"
-WMS_LOAD_PATH = "data\WMS_load.tif"
+WMS_LOAD_PATH = r"data\WMS_load.tif"
 
 ZONE_LAT = 48.198583  # 48°11'54.9"N
 ZONE_LON = 17.055028  # 17°03'18.1"E
